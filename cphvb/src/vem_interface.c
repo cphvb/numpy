e778e6d528343c18af1799ee641e46a1884728f7
Mads R. B. Kristensen <madsbk@gmail.com>
2011-11-09 13:56:20 +0100

    Added an interface to the VEM component.

---
+++ cphvb/src/vem_interface.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2011 Mads R. B. Kristensen <madsbk@gmail.com>
+ *
+ * This file is part of DistNumPy <https://github.com/distnumpy>.
+ *
+ * DistNumPy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DistNumPy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DistNumPy. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/*
+ *===================================================================
+ * Initiate the VEM interface.
+ */
+void vem_if_init(void)
+{
+    cphvb_com **coms;
+    cphvb_intp children_count;
+
+    //We are the root in the configuration.
+    self_component = cphvb_com_setup();
+
+    cphvb_com_children(self_component, &children_count, &coms);
+
+    if(children_count != 1 || coms[0]->type != CPHVB_VEM)
+    {
+        fprintf(stderr, "Error in the configuration: the bridge must "
+                        "have exactly one child of type VEM\n");
+        exit(-1);
+    }
+    vem_component = coms[0];
+    free(coms);
+    vem_init = vem_component->init;
+    vem_execute = vem_component->execute;
+    vem_shutdown = vem_component->shutdown;
+    vem_create_array = vem_component->create_array;
+
+} /* vem_if_init */
+
+
+/*
+ *===================================================================
+ * Finalize the VEM interface.
+ */
+void vem_if_finalize(void)
+{
+    cphvb_com_free(self_component);
+    cphvb_com_free(vem_component);
+
+} /* vem_if_finalize */
---
+++ cphvb/src/vem_interface.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2011 Mads R. B. Kristensen <madsbk@gmail.com>
+ *
+ * This file is part of DistNumPy <https://github.com/distnumpy>.
+ *
+ * DistNumPy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DistNumPy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DistNumPy. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef VEM_INTERFACE_H
+#define VEM_INTERFACE_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <cphvb.h>
+
+//Function pointers that makes up the VEM interface.
+cphvb_init vem_init;
+cphvb_execute vem_execute;
+cphvb_shutdown vem_shutdown;
+cphvb_create_array vem_create_array;
+cphvb_instruction_check vem_instruction_check;
+cphvb_com *self_component, *vem_component;
+
+/*
+ *===================================================================
+ * Initiate the VEM interface.
+ */
+void vem_if_init(void);
+
+/*
+ *===================================================================
+ * Finalize the VEM interface.
+ */
+void vem_if_finalize(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !defined(VEM_INTERFACE_H) */
